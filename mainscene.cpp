41cc5e2783b6b41a98d33e0b5b1260e41015e866
Felix Lapalme <lap.felix@gmail.com>
2015-04-12 12:59:24 -0400

    ajouté un espace pas utile

--- mainscene.cpp
+++ mainscene.cpp
@@ -548,7 +548,7 @@ void MainScene::sendCurrentColorToServer()
 {
 
 	//envoye les couleurs du thème au serveur
-	string str = (manager->getPresetArray().at(currentSelection).color1) + "," +
+	string str = ( manager->getPresetArray().at(currentSelection).color1) + "," +
 		(manager->getPresetArray().at(currentSelection).color2) + "," +
 		(manager->getPresetArray().at(currentSelection).color3) + "," +
 		(manager->getPresetArray().at(currentSelection).color4);
